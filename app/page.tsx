0489a62a95bc6652d1eafdf0a690f52859dbc179
emilychen <emily80108@gmail.com>
2025-06-01 20:15:59 +0800

    fix: adjust env

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 // need the force-dynamic because we don’t want Next.js to cache the pre-signed URL.
 export const dynamic = "force-dynamic";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3_BUCKET_NAME } from "@/constants/s3";
-
+import s3 from "@/utils/s3";
 import Form from "@/components/form";
 import styles from "./page.module.css";
 
@@ -15,14 +15,12 @@ export default async function Home() {
     ACL: "public-read", // 如果需要公開讀取
   });
 
-  const url = await getSignedUrl(new S3Client({}), command, {
-    expiresIn: 60 * 15, // 15 minutes
-  });
+  const presignedUrl = await getSignedUrl(s3, command, { expiresIn: 900 }); // URL 有效期 15 分钟
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Form url={url} />
+        <Form url={presignedUrl} />
       </main>
     </div>
   );
--- utils/s3.js
+++ utils/s3.js
@@ -1,5 +1,5 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { fromIni } from "@aws-sdk/credential-providers";
+// import { fromIni } from "@aws-sdk/credential-providers";
 
 // 初始化 S3 Client (Server-Side)
 const s3 = new S3Client({
