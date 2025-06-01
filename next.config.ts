b38e446171c96e2732268005ca138f14b1bdbcc4
emilychen <emily80108@gmail.com>
2025-06-01 20:02:13 +0800

    fix: adjust env

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,8 @@ const nextConfig = {
   env: {
     AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
     AWS_REGION: process.env.AWS_REGION,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     NEXT_PUBLIC_AWS_BUCKET_NAME: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME,
     NEXT_PUBLIC_AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION,
   },
